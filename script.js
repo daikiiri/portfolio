ec47cff507288bd3df82c19e76d2a10bc5409aec
Min Thuta <6531503156@lamduan.mfu.ac.th>
2025-11-24 21:35:51 +0700

    Finished

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links"); // finds the <ul>
+    const icon = document.querySelector(".hamburger-icon");
+    
+    if(menu && icon) {  // <-- safety check
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+    } else {
+        console.error("Menu or icon not found!");
+    }
+}
